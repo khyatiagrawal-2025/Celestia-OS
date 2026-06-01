e4e79718bd8230544edd22cb3285f5c267849d8f
Khyati Agrawal <khyatiagrawal9494@gmail.com>
2026-06-02 00:09:41 +0530

    Add Mission Control dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import BootScreen from "./components/BootScreen";
+import MissionControl from "./components/MissionControl";
 
 function App() {
+  const [bootComplete, setBootComplete] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setBootComplete(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <Navbar />
-      <BootScreen />
+
+      {bootComplete ? (
+        <MissionControl />
+      ) : (
+        <BootScreen />
+      )}
     </>
   );
 }
---
+++ src/components/MissionControl.jsx
@@ -0,0 +1,21 @@
+function MissionControl() {
+  return (
+    <div className="mission-control">
+      <h1>MISSION CONTROL</h1>
+
+      <div className="planet-card">
+        🌍 Earth
+      </div>
+
+      <div className="planet-card">
+        🔴 Mars
+      </div>
+
+      <div className="planet-card">
+        🪐 Saturn
+      </div>
+    </div>
+  );
+}
+
+export default MissionControl;
